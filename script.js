0cdd83c9531c9829a036c25d0b83b87e8b8f60cc
Carridice123 <carridiceraheem871@gmail.com>
2022-09-14 20:27:35 -0500

    edit buttons click function now working

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+let color = "black";
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
+    let popUp = document.querySelector("#popup");
+    popUp.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
 
 
 
 
 
-    console.log("hey");
+    
 })
 
 
@@ -21,8 +27,10 @@ function createBoard(size) {
     
     for(let i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
-        board.insertAdjacentElement("beforeend", div)
-        div.style.backgroundColor = "pink";
+        div.addEventListener("mouseover", colorDiv)
+        board.insertAdjacentElement("beforeend", div);
+        
+    
     }
 
 
@@ -30,3 +38,48 @@ function createBoard(size) {
 
 
 }
+
+function getSize() {
+    let userInput = prompt("How many boxes do you want?");
+    let message = document.querySelector(".message")
+    if(userInput === ""){
+        message.innerText = "please provide a number";
+
+    }
+    else if(userInput < 0 || userInput > 100) {
+        message.innerText = "please provide a number between one and a hundred";
+    }
+    else {
+        message.innerText = "begin your work of art";
+        return userInput;
+    }
+   
+}
+
+
+function colorDiv() {
+    if(color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = 'black';
+    }
+
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+
+
+
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
+ 
+    
+
+
+}
+
